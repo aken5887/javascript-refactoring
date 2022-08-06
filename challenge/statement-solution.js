7f444a978e665b8fbee14cc77cdf898289b304ae
aken <aken@kakao.com>
2022-08-07 00:34:09 +0900

    13.7 중간정리, 함수 분리

--- challenge/statement-solution.js
+++ challenge/statement-solution.js
@@ -18,65 +18,12 @@
  * 2. 데이터 계산 함수 / 데이터 출력 함수 분리 
  *  - 출력하고자 하는 개체들을 하나의 객체에 담기
  */
-export function statement(invoice, plays) {
-  const statement = {};
-  statement.customer = invoice.customer;
-  statement.performances = invoice.performances.map((p) => 
-    enrichPerformance(p)
-  );
-  statement.totalAmount = totalAmount(statement.performances);
-  statement.totalCredits = totalCredits(statement.performances);
-  return renderPlainText(statement, plays);
+import { createStatement } from "./create_statement.js";
 
-  function enrichPerformance(performance){
-      const result = {...performance};  // 얕은 복사
-      result.play = playFor(performance);
-      result.amount = amountFor(result);
-      result.credits = creditsFor(result);
-      return result;
-    }
-    function playFor(performance){
-      return plays[performance.playID];
-    }
-    function amountFor(performance){
-      let result = 0;
-      switch (performance.play.type) {
-        case 'tragedy': // 비극
-          result = 40000;
-          if (performance.audience > 30) {
-            result += 1000 * (performance.audience - 30);
-          }
-          break;
-        case 'comedy': // 희극
-          result = 30000;
-          if (performance.audience > 20) {
-            result += 10000 + 500 * (performance.audience - 20);
-          }
-          result += 300 * performance.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-      }
-      return result;
-    }
-    function creditsFor(performance) {
-      let result = 0;
-      result += Math.max(performance.audience - 30, 0);
-      // 희극 관객 5명마다 추가 포인트를 제공한다.
-      if ('comedy' === playFor(performance).type){
-        result += Math.floor(performance.audience / 5);
-      }
-      return result;
-    }
-    function totalAmount(performances){
-      return performances.reduce(
-        (sum, p) => (sum += p.amount), 0);
-    }
-    function totalCredits(performances){
-      return performances.reduce(
-          (sum, p) => {return sum += p.credits}, 0);
-    }
-  }
+export function statement(invoice, plays) {
+  const statement = createStatement(invoice, plays);
+  return renderPlainText(statement);
+}
 
 function renderPlainText(statement){
   let result = `청구 내역 (고객명: ${statement.customer})\n`;
